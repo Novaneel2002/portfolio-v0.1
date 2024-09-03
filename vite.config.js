ea4ba042cbe0bd7e0549ad63696d0c59f7713686
Novaneel Mandal <novaneel2802@gmail.com>
2024-09-03 22:31:55 +0530

    please

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-v1.0/"
+  base: "/portfolio-v1.0/",
+  optimizeDeps: {
+    include: ['bootstrap/dist/css/bootstrap.min.css']
+  }
 })
